fa6453e5b9a649dca53ab0b41904e7f552f1fc55
DaviGel <geloso.davide@gmail.com>
2023-10-31 16:13:01 +0100

    fix: numero casuale da 1 a 1000

--- myscript.js
+++ myscript.js
@@ -1,7 +1,7 @@
 const nomeUtente = prompt('Inserisci il tuo nome:');
 const cognomeUtente = prompt('Inserisci il tuo cognome:');
 const colorePreferito = prompt('Inserisci il tuo colore preferito:');
-const numero = Math.floor(Math.random() * 100 + 1);
+const numero = Math.floor(Math.random() * 1000 + 1);
 
 console.log(
   (document.getElementById('titolo').innerHTML =
